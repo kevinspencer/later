3e7622a635ebb91db77a006a652dfb7478fcf8b5
Kevin Spencer <kevin@kevinspencer.org>
2019-03-01 16:20:07 -0700

    and so it begins

---
+++ server/app.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+# Copyright 2019 Kevin Spencer <kevin@kevinspencer.org>
+#
+# Permission to use, copy, modify, distribute, and sell this software and its
+# documentation for any purpose is hereby granted without fee, provided that
+# the above copyright notice appear in all copies and that both that
+# copyright notice and this permission notice appear in supporting
+# documentation. No representations are made about the suitability of this
+# software for any purpose. It is provided "as is" without express or
+# implied warranty.
+#
+################################################################################
+
+use Mojolicious::Lite;
+use strict;
+use warnings;
+
+our $VERSION = '0.1';
+
+get '/queue' => sub {
+    my $c = shift;
+    $c->render(
+        json => [
+            { id => 2, timestamp => 123456789, text => "Coffee.  Who's with me?" }
+        ],
+    );
+};
+
+app->start;
