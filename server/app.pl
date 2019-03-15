8e8781e7cdeb942df63cb6ee24271391c79c11da
Kevin Spencer <kevin@kevinspencer.org>
2019-03-15 05:46:46 -0700

    initial queue writing

--- server/app.pl
+++ server/app.pl
@@ -13,13 +13,15 @@
 
 use Cwd;
 use Data::Dumper;
+use File::Path;
 use Mojolicious::Lite;
 use Text::CSV_XS;
+use Try::Tiny;
 use strict;
 use warnings;
 
 $Data::Dumper::Indent = 1;
-our $VERSION = '0.5';
+our $VERSION = '0.6';
 
 my $queue_dir  = getcwd() . '/queue';
 my $queue_file = $queue_dir . '/later.queue';
@@ -61,6 +63,13 @@ post '/queue' => sub {
 
     my $later_hash = $c->req->json();
     if (($later_hash) && (exists($later_hash->{tweet}))) {
+        if (! -d $queue_dir) {
+            try {
+                mkpath($queue_dir);
+            } catch {
+                print $_, "\n";
+            };
+        }
         $c->render(text => 'yes');
     } else {
         $c->render(text => 'nope');
