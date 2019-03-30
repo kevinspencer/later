4148223166691f34545ebc8792e262d5be42c736
Kevin Spencer <kevin@kevinspencer.org>
2019-03-29 20:18:20 -0700

    and so it begins

---
+++ cron/post-from-queue.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+# Copyright 2019 Kevin Spencer <kevin@kevinspencer.org>
+#
+# Permission to use, copy, modify, distribute, and sell this software and its
+# documentation for any purpose is hereby granted without fee, provided that
+# the above copyright notice appear in all copies and that both that
+# copyright notice and this permission notice appear in supporting
+# documentation. No representations are made about the suitability of this
+# software for any purpose. It is provided "as is" without express or
+# implied warranty.
+#
+################################################################################
+
+use Data::Dumper;
+use utf8;
+use strict;
+use warnings;
+
+$Data::Dumper::Indent = 1;
+
+our $VERSION = '0.1';
